69d53ccfae0e62297774ce38c0f85ebd358b993a
H-H2648 <46250599+H-H2648@users.noreply.github.com>
2020-06-18 12:17:06 -0700

    theres grid now
    added grid
    
    the points are not a big black circle now

--- src/board.js
+++ src/board.js
@@ -2,11 +2,19 @@ import React from 'react';
 
 import './App.css';
 import Point from './point.js';
+import Square from './square.js';
 
 export default class Board extends React.Component {
 
   
-
+  renderSquare(ii, jj){
+    return <Square
+      piece = {this.props.points[ii][jj]}
+      style = {this.props.points[ii][jj]? this.props.points[ii][jj].style : null}
+      onClick={() => this.props.onClick(ii, jj)} 
+    />
+  }
+  
   renderPoint(ii, jj) {
     return <Point
       piece = { this.props.points[ii][jj] } 
@@ -15,12 +23,18 @@ export default class Board extends React.Component {
     />
   }
 
+
+
   render() {
     var board = []
     for(let ii = 0; ii < 10; ++ii){
       var boardRows = [];
       for(let jj = 0; jj < 9; ++jj){
+        /*
         boardRows.push(this.renderPoint(ii, jj));
+        */
+       boardRows.push(this.renderPoint(ii, jj));
+       //boardRows.push(this.renderPoint(ii, jj));
       }
       board.push(<div className="board-row">{boardRows}</div>)
     }
--- src/point.js
+++ src/point.js
@@ -3,10 +3,27 @@ import './App.css';
 
 export default function Point(props) {
     return (
-      <button className={"point"}
-      onClick={props.onClick}
-      style={props.style}>
-      </button>
+
+
+      <button className = "square">
+        <div className="point"
+        onClick={props.onClick}
+        style={props.style}>
+        </div> 
+      </button> 
+    
     );
-  
 }
+
+/*
+<button className="point"
+        onClick={props.onClick}
+        style={props.style}>
+      </button> 
+*/
+
+/*
+<div className = "square">
+      
+      </div>
+*/
